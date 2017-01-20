9a709f2aff9b3177c46de0b5d1c6b89d9395bd4a
LancerComet <chw644@hotmail.com>
2017-01-20 10:43:54 +0800

    + Using CommonJS module.

--- src/index.js
+++ src/index.js
@@ -7,36 +7,26 @@
    */
 
 let inited = false
-let vm = null
 
-;(function(root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    define([], factory);
-  } else if (typeof exports === 'object') {
-    module.exports = factory();
-  } else {
-    root.Index = factory();
-  }
-}(this, function () {
-  const vuexer = {
-    install (Vue, options) {
-      if (!options || !options.store || !options.actions || !options.getters) {
-        window.console && console.warn('[Vuexer] You must provide Vuex modules for Vuexer initialization.')
-        return
-      }
-      if (!inited) {
-        vm = new Vue({
-          store: options.store,
-          vuex: {
-            actions: options.actions,
-            getters: options.getters
-          }
-        })
-        Vue.vuexer = vm
-        Vue.prototype.$vuexer = vm
-        inited = true
-      }
+const vuexer = {
+  install (Vue, options) {
+    if (!options || !options.store || !options.actions || !options.getters) {
+      window.console && console.warn('[Vuexer] You must provide Vuex modules for Vuexer initialization.')
+      return
+    }
+    if (!inited) {
+      const vm = new Vue({
+        store: options.store,
+        vuex: {
+          actions: options.actions,
+          getters: options.getters
+        }
+      })
+      Vue.vuexer = vm
+      Vue.prototype.$vuexer = vm
+      inited = true
     }
   }
-  return vuexer
-}));
+}
+
+module.exports = vuexer
